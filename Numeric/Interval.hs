2c5dce59c0dbb1aa9a7692892da73a81c9d31164
ekmett <ekmett@gmail.com>
2010-06-13 17:31:32 -0700

    successfully calculates
    Ignore-this: b0e27578dcfc46f32c405dfd4b90d115
    
    darcs-hash:20100614003132-7b3c0-15afd27b2cd04f748fe7ed0b5205579fd61fa7df.gz

--- Numeric/Interval.hs
+++ Numeric/Interval.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Rank2Types, TypeFamilies #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Numeric.Interval
@@ -11,46 +12,331 @@
 --
 -----------------------------------------------------------------------------
 
-module Numeric.Interval
+module Numeric.Interval 
+    ( whole
+    , empty
+    , null
+    , singleton
+    , inf
+    , sup
+    , singular
+    , width
+    , intersection
+    , hull
+    , bisection
+    , magnitude
+    , mignitude
+    , certainly, (<!), (<=!), (==!), (>=!), (>!)
+    , possibly, (<?), (<=?), (==?), (>=?), (>?)
+    ) where
 
-import Numeric.AD.Classes
+import Prelude hiding (null)
+import Numeric.Extras
+import Numeric.Rounding
+import Data.Function (on)
 
 data Interval a = I (Round Down a) (Round Up a)
 
-instance Eq a => Eq (Interval a) where
-    I a b == I a' b' = a == a' && b == b'
+infix 3 ...
+
+-- | The rule of thumb is you should only use this to construct using values
+-- that you took out of the interval. Otherwise, use I, to force rounding
+(...) :: a -> a -> Interval a 
+a ... b = I (Round a) (Round b)
+{-# INLINE (...) #-}
+
+-- | The whole real number line
+whole :: Precision a => Interval a 
+whole = (-1)/0 ... 1/0
+{-# INLINE whole #-}
+
+-- | An empty interval
+empty :: Precision a => Interval a 
+empty = 0/0 ... 0/0
+{-# INLINE empty #-}
+
+-- | negation handles NaN properly
+null :: Ord a => Interval a -> Bool
+null x = not (inf x <= sup x)
+{-# INLINE null #-}
+
+-- | A singleton point
+singleton :: a -> Interval a 
+singleton a = a ... a
+{-# INLINE singleton #-}
+
+-- | The infinumum (lower bound) of an interval
+inf :: Interval a -> a
+inf (I (Round a) _) = a
+{-# INLINE inf #-}
+
+-- | The supremum (upper bound) of an interval
+sup :: Interval a -> a
+sup (I _ (Round b)) = b
+{-# INLINE sup #-}
+
+-- | Is the interval a singleton point? 
+-- N.B. This is fairly fragile and likely will not hold after
+-- even a few operations that only involve singletons
+singular :: Ord a => Interval a -> Bool
+singular x = not (null x) && inf x == sup x
+{-# INLINE singular #-}
+
+instance Precision a => Eq (Interval a) where
+    (==) = (==) `on` midpoint
 
 instance Show a => Show (Interval a) where
-    showsPrec n (Interval a b) =   
-        showChar '[' . 
-        showsPrec 0 a .
-        showString ".." .
-        showsPrec 0 b .
-        showChar ']'
+    showsPrec n (I (Round a) (Round b)) =   
+        showParen (n > 3) $
+            showsPrec 3 a .
+            showString " ... " . 
+            showsPrec 3 b
 
+-- flip the rounding mode up
 u :: Round Down a -> Round Up a
 u (Round a) = Round a
+{-# INLINE u #-}
 
+-- flip the rounding mode down
 d :: Round Up a -> Round Down a
 d (Round a) = Round a
+{-# INLINE d #-}
+
+-- | Calculate the width of an interval.
+-- N.B. the width of an interval is an interval itself due to rounding
+width :: Precision a => Interval a -> Interval a
+width x@(I a b) = I (d b - a) (b - u a)
+{-# INLINE width #-}
+
+-- | magnitude 
+magnitude :: Precision a => Interval a -> a 
+magnitude x = (max `on` abs) (inf x) (sup x)
+
+-- | "mignitude"
+mignitude :: Precision a => Interval a -> a 
+mignitude x = (min `on` abs) (inf x) (sup x)
 
 instance Precision a => Num (Interval a) where
     I a b + I a' b' = I (a + a') (b + b')
     I a b - I a' b' = I (a - d b') (b - u a')
-    I a b * I a' b' = I (minimum [a*a',a*d b',d b*a',d b * d b')])
-                        (maximum [u a*u a',u a*b',b*u a',b*b'])
-    abs (I a b) = I (on min abs a (d b)) (on max abs (u a) b)
-    signum (I a b) = I (signum a) (signum b)
-    fromInteger i = I (fromInteger i) (fromInteger i)
+    I a b * I a' b' = minimum [a * a',a * d b',d b * a',d b * d b'] 
+                      `I` 
+                      maximum [u a * u a',u a * b',b * u a',b * b']
+    abs x@(I a b) 
+        | a >= 0    = x 
+        | b <= 0    = negate x
+        | otherwise = max (- a) (d b) `I` b
+
+    signum (I a b)  = signum a `I` signum b
+
+    fromInteger i   = fromInteger i `I` fromInteger i
+
+-- | Bisect an interval at its midpoint.
+bisection :: Precision a => Interval a -> (Interval a, Interval a)
+bisection (I a b) = (I a (u a + (b - u a) / 2), I (a + (d b - a) / 2) b)
+{-# INLINE bisection #-}
+
+-- | Nearest point to the midpoint of the interval.
+midpoint :: Precision a => Interval a -> a
+midpoint x = inf x + (sup x - inf x) / 2
+{-# INLINE midpoint #-}
+
+-- | This means that realToFrac will use the midpoint
+instance Precision a => Real (Interval a) where
+    toRational x = toRational (midpoint x)
 
 instance Precision a => Fractional (Interval a) where
-    I a b * I a' b' = I (minimum [a/a',a/d b',d b/a',d b/d b'])
-                        (maximum [u a/u a',u a/b',b/u a',b/b'])
-    recip (I a b) = I (on min recip a (d b)) (on max recip (u a) b)
-    fromRational r = I (fromRational r) (fromRational r)
-    
+    I a b / I a' b' = minimum [a / a',a / d b',d b / a',d b / d b'] 
+                      `I`
+                      maximum [u a / u a',u a / b',b / u a',b / b']
+    recip (I a b)   = on min recip a (d b) `I` on max recip (u a) b
+    fromRational r  = fromRational r `I` fromRational r
+
+instance Precision a => RealFrac (Interval a) where
+    properFraction x = (b, x - fromIntegral b)
+        where 
+            b = truncate (midpoint x)
+    ceiling x = ceiling (sup x)
+    floor x = floor (inf x)
+    round x = round (midpoint x)
+    truncate x = truncate (midpoint x)
+
 instance Precision a => Floating (Interval a) where
-    pi = I pi pi 
-    exp (I a b) = I (exp a) (exp b)
-    log (I a b) + I (if a > 0 then log a else -1/0) (log b)
+    pi = pi `I` pi 
+    exp (I a b) = exp a `I` exp b
+    log (I a b) = (if a > 0 then log a else -1/0) `I` log b
+    cos x 
+        | inf (width t) >= inf pi = (-1) ... 1
+        | u tl >= pih  = - cos (t - pi)
+        | d th <= pil  = cos (d th) `I` cos (u tl)
+        | d th <= pi2l = (-1) `I` cos (u (min (pi2l - d th) tl))
+        | otherwise  = (-1) ... 1
+        where 
+            I pil pih = pi
+            pi2@(I pi2l pi2h) = pi * 2
+            t@(I tl th) = x `fmod` pi2
+            l = inf t
+            h = sup t
+
+-- | We have to play some semantic games to make these methods make sense.
+-- Most compute with the midpoint of the interval.
+instance Precision a => RealFloat (Interval a) where
+    floatRadix = floatRadix . midpoint
+    floatDigits = floatDigits . midpoint
+    floatRange = floatRange . midpoint
+    decodeFloat = decodeFloat . midpoint
+    encodeFloat m e = singleton (encodeFloat m e)
+    exponent = exponent . midpoint
+    significand x = min a b ... max a b
+        where
+            (mm,em) = decodeFloat (midpoint x)
+            (mi,ei) = decodeFloat (inf x)
+            (ms,es) = decodeFloat (sup x)
+            a = encodeFloat mi (ei - em - floatDigits x) 
+            b = encodeFloat ms (es - em - floatDigits x)
+    scaleFloat n x = scaleFloat n (inf x) ... scaleFloat n (sup x)
+    isNaN x = isNaN (inf x) || isNaN (sup x)
+    isInfinite x = isInfinite (inf x) || isInfinite (sup x)
+    isDenormalized x = isDenormalized (inf x) || isDenormalized (sup x)
+    -- contains negative zero
+    isNegativeZero x = not (inf x > 0) 
+                    && not (sup x < 0)
+                    && (  (sup x == 0 && (inf x < 0 || isNegativeZero (inf x)))
+                       || (inf x == 0 && isNegativeZero (inf x)) 
+                       || (inf x < 0 && sup x >= 0))
+    atan2 = error "unimplemented"
+
+-- TODO: (^), (^^) to give tighter bounds
+        
+-- | Calculate the intersection of two intervals.
+intersection :: Precision a => Interval a -> Interval a -> Interval a
+intersection x@(I a b) y@(I a' b')
+    | x /=! y = empty
+    | otherwise = I (max a a') (min b b')
+
+-- | Calculate the convex hull of two intervals
+hull :: Ord a => Interval a -> Interval a -> Interval a
+hull x@(I a b) y@(I a' b') 
+    | null x = y
+    | null y = x
+    | otherwise = I (min a a') (max b b')
     
+instance Precision a => RealExtras (Interval a) where
+    type C (Interval a) = C a
+    -- output always lies within the interval y if y >=! 0
+    fmod x y | null y = empty 
+             | inf y >= 0 = r -- `intersection` bounds
+        where 
+            n = floor (inf x / if inf x < 0 then inf y else sup y)
+            r = x - fromIntegral n * y 
+            bounds | inf y >= 0 = y
+                   | otherwise = y `hull` negate y
+    
+    
+-- | For all @x@ in @X@, @y@ in @Y@. @x '<' y@
+(<!)  :: Ord a => Interval a -> Interval a -> Bool
+x <! y = sup x < inf y
+{-# INLINE (<!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x '<=' y@
+(<=!) :: Ord a => Interval a -> Interval a -> Bool
+x <=! y = sup x <= inf y
+{-# INLINE (<=!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x '==' y@
+(==!) :: Eq a => Interval a -> Interval a -> Bool
+x ==! y = sup x == inf y && inf x == sup y
+{-# INLINE (==!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x '/=' y@
+(/=!) :: Ord a => Interval a -> Interval a -> Bool
+x /=! y = sup x < inf y || inf x > sup y
+{-# INLINE (/=!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x '>' y@
+(>!)  :: Ord a => Interval a -> Interval a -> Bool
+x >! y = inf x > sup y
+{-# INLINE (>!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x '>=' y@
+(>=!) :: Ord a => Interval a -> Interval a -> Bool
+x >=! y = inf x >= sup y
+{-# INLINE (>=!) #-}
+
+-- | For all @x@ in @X@, @y@ in @Y@. @x `op` y@
+certainly :: Ord a => (forall b. Ord b => b -> b -> Bool) -> Interval a -> Interval a -> Bool
+certainly cmp l r 
+    | lt && eq && gt = True
+    | lt && eq       = l <=! r
+    | lt &&       gt = l /=! r
+    | lt             = l <! r 
+    |       eq && gt = l >=! r 
+    |       eq       = l ==! r
+    |             gt = l >! r
+    | otherwise      = False
+    where 
+        lt = cmp LT EQ
+        eq = cmp EQ EQ
+        gt = cmp GT EQ
+{-# INLINE certainly #-}
+
+contains :: Ord a => Interval a -> Interval a -> Bool
+contains x y = null y 
+            || (not (null x) && inf x <= inf y && sup y <= sup x)
+
+-- | Comparisons are made on the midpoint
+instance Precision a => Ord (Interval a) where
+    compare = compare `on` midpoint
+    max (I a b) (I a' b') = I (max a a') (max b b')
+    min (I a b) (I a' b') = I (min a a') (min b b')
+
+ambiguous :: String -> a
+ambiguous s = error $ s ++ ": ambiguous result"
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '<' y@?
+(<?) :: Ord a => Interval a -> Interval a -> Bool
+x <? y = inf x < sup y
+{-# INLINE (<?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '<=' y@?
+(<=?) :: Ord a => Interval a -> Interval a -> Bool
+x <=? y = inf x <= sup y
+{-# INLINE (<=?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '==' y@?
+(==?) :: Ord a => Interval a -> Interval a -> Bool
+x ==? y = inf x <= sup y && sup x >= inf y
+{-# INLINE (==?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '/=' y@?
+(/=?) :: Eq a => Interval a -> Interval a -> Bool
+x /=? y = inf x /= sup y || sup x /= inf y
+{-# INLINE (/=?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '>' y@?
+(>?) :: Ord a => Interval a -> Interval a -> Bool
+x >? y = sup x > inf y
+{-# INLINE (>?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x '>=' y@?
+(>=?) :: Ord a => Interval a -> Interval a -> Bool
+x >=? y = sup x >= inf y
+{-# INLINE (>=?) #-}
+
+-- | Does there exist an @x@ in @X@, @y@ in @Y@ such that @x `op` y@?
+possibly :: Ord a => (forall b. Ord b => b -> b -> Bool) -> Interval a -> Interval a -> Bool
+possibly cmp l r 
+    | lt && eq && gt = True
+    | lt && eq       = l <=? r
+    | lt &&       gt = l /=? r
+    | lt             = l <? r 
+    |       eq && gt = l >=? r 
+    |       eq       = l ==? r
+    |             gt = l >? r
+    | otherwise      = False
+    where 
+        lt = cmp LT EQ
+        eq = cmp EQ EQ
+        gt = cmp GT EQ
+{-# INLINE possibly #-}
+
