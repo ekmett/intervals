f79df555f6cc817c9c3e4f7de2338c2713929245
Edward Kmett <ekmett@gmail.com>
2013-10-29 14:59:29 -0400

    Add an HLint configuration

---
+++ HLint.hs
@@ -0,0 +1,12 @@
+import "hint" HLint.HLint
+
+-- not viable
+ignore "Reduce duplication"
+
+-- don't want to!
+ignore "Use infix"
+
+-- these don't consider the corner cases when using doubles
+ignore "Use >"
+ignore "Use <="
+ignore "Use >="
